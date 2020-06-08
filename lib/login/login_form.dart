c9df60bfbe0777c64f5d1d5223c6e2be34da74eb
Jack Patterson <jtyler03@optonline.net>
2020-06-08 13:40:42 -0400

    commit

--- lib/login/login_form.dart
+++ lib/login/login_form.dart
@@ -16,17 +16,14 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    
-
     Color color = Theme.of(context).backgroundColor;
 
-
-    
     getColor() {
       print(color);
       return color;
     }
-  invertBackground() {
+
+    invertBackground() {
       if (getColor() == Colors.black) {
         return Color.fromRGBO(241, 243, 246, 1);
       } else {
@@ -34,7 +31,7 @@ class _LoginFormState extends State<LoginForm> {
       }
     }
 
-        invertShadow() {
+    invertShadow() {
       if (getColor() == Colors.black) {
         return Color.fromRGBO(0, 0, 0, .25);
       } else {
@@ -61,121 +58,167 @@ class _LoginFormState extends State<LoginForm> {
       child: BlocBuilder<LoginBloc, LoginState>(
         builder: (context, state) {
           return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.only(left: 16,right: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-             
-              Column(
-                 
-                crossAxisAlignment: CrossAxisAlignment.start,
-                
-                children: <Widget>[
-                  Center(child:  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Container(
-                height: 70,
-                width: 70,
-                decoration: BoxDecoration(color: invertBackground(), borderRadius: BorderRadius.all(Radius.circular(15)), boxShadow: [
-                  BoxShadow(color: invertShadow(), offset: Offset(30, 30), blurRadius: 60)
-                ]),
-              child: Padding(padding: EdgeInsets.all(10), child: Image.asset('assets/icon.png',))),
-                  ), ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Text("Welcome Back",style: TextStyle(fontSize: 26, fontFamily: 'Poppins-Light', ),),
-                  ),
-                  Text("Sign in",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold, ),),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: "Username",
-                      labelStyle: TextStyle(fontSize: 14,color: Colors.grey.shade400),
-                      
-                    ),
-                    controller: _usernameController,
-
-                  ),
-                  SizedBox(height: 16,),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      labelStyle: TextStyle(fontSize: 14,color: Colors.grey.shade400),
-                    ),
-                    controller: _passwordController,
-
-                  ),
-                  SizedBox(height: 12,),
-                   Padding(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            body: SafeArea(
+              child: Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    GestureDetector(
-                      onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                          return ForgotPass();
-                        }));
-                      },
-                      child: Text("Forgot Password",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red),),
-                    )
-                        ],
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 30),
+                            child: Container(
+                                height: 70,
+                                width: 70,
+                                decoration: BoxDecoration(
+                                    color: invertBackground(),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(15)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: invertShadow(),
+                                          offset: Offset(30, 30),
+                                          blurRadius: 60)
+                                    ]),
+                                child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Image.asset(
+                                      'assets/icon.png',
+                                    ))),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 30),
+                          child: Text(
+                            "Welcome Back",
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontFamily: 'Poppins-Light',
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "Sign in",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                  SizedBox(height: 30,),
-                  Container(
-                    height: 50,
-                    width: double.infinity,
-                    child: FlatButton(
-                      onPressed: state is! LoginLoading
-                        ? _onLoginButtonPressed
-                        : null,
-                      padding: EdgeInsets.all(0),
-                      child: Ink(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(6),
-                          
-                          gradient: LinearGradient(
+                    Column(
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Username",
+                            labelStyle: TextStyle(
+                                fontSize: 14, color: Colors.grey.shade400),
+                          ),
+                          controller: _usernameController,
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
                             
-                            
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                            colors: [
-                              Color(0xff8A2387),
-                              Color(0xffE94057),
-                              Color(0xffF27121),
+                            labelText: "Password",
+                            labelStyle: TextStyle(
+                                fontSize: 14, color: Colors.grey.shade400),
+                          ),
+                          controller: _passwordController,
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) {
+                                    return ForgotPass();
+                                  }));
+                                },
+                                child: Text(
+                                  "Forgot Password",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.red),
+                                ),
+                              )
                             ],
                           ),
                         ),
-                        child: Container(
-                          alignment: Alignment.center,
-                          constraints: BoxConstraints(maxWidth: double.infinity,minHeight: 50),
-                          child: Text("Login",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
+                        SizedBox(
+                          height: 30,
                         ),
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6),
-                      ),
+                        Container(
+                          height: 50,
+                          width: double.infinity,
+                          child: FlatButton(
+                            onPressed: state is! LoginLoading
+                                ? _onLoginButtonPressed
+                                : null,
+                            padding: EdgeInsets.all(0),
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                gradient: LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  colors: [
+                                    Color(0xff8A2387),
+                                    Color(0xffE94057),
+                                    Color(0xffF27121),
+                                  ],
+                                ),
+                              ),
+                              child: Container(
+                                alignment: Alignment.center,
+                                constraints: BoxConstraints(
+                                    maxWidth: double.infinity, minHeight: 50),
+                                child: Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(6),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        GestureDetector(
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Register(),
+                                )),
+                            child: Text(
+                              "Register",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red),
+                            )),
+                        SizedBox(
+                          height: 46,
+                        ),
+                      ],
                     ),
-                  ),
-                                    SizedBox(height: 10,),
-
-                  GestureDetector(onTap: () => Navigator.push(
-                                                        context,
-                                                        MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              Register(),
-                                                        )), child: Text("Register",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red),)),
-                  SizedBox(height: 46,),
-                ],
-              ),
-              
                   ],
                 ),
               ),
--- lib/login/register.dart
+++ lib/login/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class Register extends StatefulWidget {
   @override
@@ -6,13 +7,50 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+
+
+double _topPadding = 200;
+
+          _changePadding() {
+      setState(() {
+        _topPadding = 100;
+      });
+    
+  }
+
+    @override
+    void initState() {
+      super.initState();
+      _topPadding = 200;
+          Timer(const Duration(milliseconds: 1000), () {
+            _changePadding();
+          });
+
+    }
+
+
   @override
   Widget build(BuildContext context) {
+    
+
+
     return Material(
       child: Scaffold(
-        body: Stack(children: <Widget>[
-          AnimatedPositioned(child: Container(decoration: BoxDecoration(color: Colors.white)),),
-        ],),
+        body: Stack(
+          children: <Widget>[
+            AnimatedPadding(
+              padding: EdgeInsets.only(top: _topPadding),
+              duration: Duration(milliseconds: 500),
+              child: AnimatedContainer(
+                  duration: Duration(
+                    milliseconds: 500,
+                  ),
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(color: Colors.white)),
+            ),
+          ],
+        ),
       ),
     );
   }
